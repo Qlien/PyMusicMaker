e6be0a21d7285614c28eb93345e91f0841e12357
mb <bakalarskimichal@gmail.com>
2018-01-31 01:23:34 +0100

    plugins begining

--- Frames/plugins_tree_frame.py
+++ Frames/plugins_tree_frame.py
@@ -1,11 +1,12 @@
 import os
 import wx
+import Plugins
 
 
 def generate_plugins_frame(parent):
     win = wx.MDIChildFrame(parent, -1, "Plugins", size=(110,600), pos=(0,0), style=wx.DEFAULT_FRAME_STYLE ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX)
     s = wx.BoxSizer(wx.VERTICAL)
-    s.Add(MineDrop(win), 1, wx.EXPAND)
+    s.Add(MineDrop(win, parent), 1, wx.EXPAND)
     win.SetSizer(s)
     win.SetSizeHints(110,600, 1200, 1200)
     win.Show(True)
@@ -21,7 +22,8 @@ class MyTextDropTarget(wx.TextDropTarget):
 
 
 class MineDrop(wx.Panel):
-    def __init__(self, parent):
+    def __init__(self, parent, frameParent):
+        self.frameParent = frameParent
         wx.Panel.__init__(self, parent)
         self.tree_ctrl = wx.TreeCtrl(self, -1, style=wx.TR_DEFAULT_STYLE)
         s = wx.BoxSizer(wx.VERTICAL)
@@ -30,16 +32,29 @@ class MineDrop(wx.Panel):
 
         # Add the tree root
         root = self.tree_ctrl.AddRoot('Instruments')
-
-        for i in range(10):
-            self.tree_ctrl.AppendItem(root, 'Item %d' % (i + 1))
-
-            self.tree_ctrl.ExpandAll()
+        dir_path = os.getcwd()
+        for path, dirs, files in os.walk(dir_path + '/Plugins'):
+            print(path)
+            for f in files:
+                print(f)
+        print(dir_path)
+        self.tree_ctrl.AppendItem(root, 'Oscillator')
+        image_list = wx.ImageList(100, 50)
+        #OscillatorImg = image_list.Add(wx.Image("images/grooveshark (Custom).png", wx.BITMAP_TYPE_PNG).Scale(100, 50).ConvertToBitmap())
+        self.tree_ctrl.ExpandAll()
         self.Centre()
         self.tree_ctrl.Bind(wx.EVT_TREE_BEGIN_DRAG, self.OnDragInit)
+        self.tree_ctrl.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnActivated)
 
     def OnDragInit(self, event):
-        my_data = wx.TextDataObject("This text will be dragged.")
-        dragSource = wx.DropSource(self)
-        dragSource.SetData(my_data)
-        result = dragSource.DoDragDrop(True)
+        text = self.tree_ctrl.GetItemText(event.GetItem())
+
+        tdo = wx.TextDataObject(text)
+        tds = wx.DropSource(self.tree_ctrl)
+        tds.SetData(tdo)
+        tds.DoDragDrop(True)
+
+
+    def OnActivated(self, event):
+        win = wx.MDIChildFrame(self.frameParent, -1, "Plugins",
+                               style=wx.DEFAULT_FRAME_STYLE ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX)
---
+++ Plugins/Instruments/Oscillator/main.py
@@ -0,0 +1,11 @@
+import wx
+
+
+class Plugin:
+    def __init__(self):
+        self.icon = wx.Bitmap('Graphics\icon.png')
+        pass
+
+class MainPlugin(Plugin):
+    def __init__(self):
+        pass
