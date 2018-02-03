40a0dffaecf180fe691286a2874d204426fba9d2
mb <bakalarskimichal@gmail.com>
2018-02-03 20:04:57 +0100

    oscillator plugin layout made

--- Plugins/Oscillator.py
+++ Plugins/Oscillator.py
@@ -2,86 +2,100 @@ import wx
 import random
 from plugin import PluginBase, PluginType
 import wx.lib.agw.knobctrl as KC
+import wx.lib.colourselect as csel
 
 
-class Oscillator(PluginBase, wx.Panel):
+class Oscillator(PluginBase):
     icon = wx.Bitmap('Plugins\Oscillator\Graphics\icon.png')
     pluginType = PluginType.SOUNDGENERATOR
     def __init__(self, frameParent):
         super(Oscillator, self).__init__(frameParent, PluginType.SOUNDGENERATOR, wx.Bitmap('Plugins\Oscillator\Graphics\icon.png'))
 
-        win = wx.MDIChildFrame(self.frameParent, -1, "Oscillator", size=(600,400),
-                               style=wx.DEFAULT_FRAME_STYLE ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX)
+        self.knob1 = KC.KnobCtrl(self, -1, size=(100, 100))
+        self.knob2 = KC.KnobCtrl(self, -1, size=(100, 100))
+        self.knob3 = KC.KnobCtrl(self, -1, size=(100, 100))
 
-        super(wx.Panel, self).__init__(win)
-        icn = wx.Icon()
-        icn.CopyFromBitmap(self.icon)
-        win.SetIcon(icn)
-        self.menuElementHeight = 30
-        
-        menuPanel = wx.Panel(self, -1)
-        pluginPanel = wx.Panel(self, -1)
-        
-        windowSizer = wx.BoxSizer(wx.VERTICAL)
-        menuSizer = wx.BoxSizer(wx.HORIZONTAL)
-        pluginSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.knob1.SetTags(range(0, 101, 5))
+        self.knob1.SetAngularRange(-45, 225)
+        self.knob1.SetValue(50)
 
-        instrumentNameText = wx.StaticText(menuPanel, id=-1, label="Instrument Name")
-        instrumentNameTextCtrl = wx.TextCtrl(menuPanel, id=-1)
+        self.knob2.SetTags(range(0, 101, 5))
+        self.knob2.SetAngularRange(-45, 225)
+        self.knob2.SetValue(50)
 
-        instrumentColorText = wx.StaticText(menuPanel, id=-1, label="Instrument Color")
-        instrumentColorPicker = wx.ColourPickerCtrl(menuPanel, id=-1, colour=wx.Colour(random.randint(0,255), random.randint(0,255), random.randint(0,255), alpha=255))
-        
-        saveButton = wx.Button(menuPanel, -1, "Save")
+        self.knob3.SetTags(range(0, 101, 5))
+        self.knob3.SetAngularRange(-45, 225)
+        self.knob3.SetValue(50)
 
-        menuSizer.Add(instrumentNameText, -1, wx.ALIGN_LEFT)
-        menuSizer.Add(instrumentNameTextCtrl, -1, wx.ALIGN_LEFT)
-        menuSizer.Add(instrumentColorText, -1, wx.ALIGN_LEFT)
-        menuSizer.Add(instrumentColorPicker, -1, wx.ALIGN_LEFT)
-        menuSizer.Add(saveButton, -1, wx.ALIGN_LEFT)
-        menuPanel.SetSizer(menuSizer)
-        
-        knob1 = KC.KnobCtrl(pluginPanel, -1, size=(100, 100))
-        pluginSizer.Add(knob1, -1, wx.ALIGN_LEFT)
+        self.knobtracker1 = wx.StaticText(self, -1, "Value = " + str(self.knob1.GetValue()))
+        self.knobtracker2 = wx.StaticText(self, -1, "Value = " + str(self.knob2.GetValue()))
+        self.knobtracker3 = wx.StaticText(self, -1, "Value = " + str(self.knob3.GetValue()))
 
+        leftknobsizer_staticbox = wx.StaticBox(self, -1, "Play With Me!")
+        middleknobsizer_staticbox = wx.StaticBox(self, -1, "Change My Properties!")
+        tightknobsizer_staticbox = wx.StaticBox(self, -1, "Change My Properties!3")
 
-        
-        windowSizer.Add(menuPanel, -1, wx.EXPAND)
-        windowSizer.Add(pluginPanel, -1, wx.EXPAND)
-        self.SetSizer(windowSizer)
-        #hbox1 = wx.BoxSizer(wx.HORIZONTAL)
-        #l1 = wx.StaticText(self, -1, "Text Field")
+        menusizer_staticbox = wx.StaticBox(self, -1, "Menu")
 
-        #hbox1.Add(l1, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        #self.t1 = wx.TextCtrl(self)
+        panelsizer = wx.BoxSizer(wx.VERTICAL)
+        menusizer = wx.BoxSizer(wx.HORIZONTAL)
+        bottomsizer = wx.BoxSizer(wx.HORIZONTAL)
+        menuStaticSizer = wx.StaticBoxSizer(menusizer_staticbox, wx.HORIZONTAL)
+        leftknobsizer = wx.StaticBoxSizer(leftknobsizer_staticbox, wx.VERTICAL)
+        middleknobsizer = wx.StaticBoxSizer(middleknobsizer_staticbox, wx.VERTICAL)
+        rightknobsizer = wx.StaticBoxSizer(tightknobsizer_staticbox, wx.VERTICAL)
 
-        #hbox1.Add(self.t1, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
-        #hbox1.Add(wx.TextCtrl(self), 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        instrumentNameText = wx.StaticText(self, id=-1, label="Name:")
+        instrumentNameTextCtrl = wx.TextCtrl(self, id=-1, value=self.pluginName)
 
-        #knob1 = KC.KnobCtrl(self, -1, size=(100, 100))
-        #knob2 = KC.KnobCtrl(self, -1, size=(100, 100))
-        #knob3 = KC.KnobCtrl(self, -1, size=(100, 100))
+        instrumentColorText = wx.StaticText(self, id=-1, label="Color:")
+        instrumentColorPicker = wx.ColourPickerCtrl(self, id=-1,
+                                                    colour=wx.Colour(random.randint(0, 255), random.randint(0, 255),
+                                                                     random.randint(0, 255), alpha=255))
 
-        #knob1.SetTags(range(0, 151, 10))
-        #knob1.SetAngularRange(-45, 225)
-        #knob1.SetValue(45)
+        saveButton = wx.Button(self, -1, "Save")
 
-        #knob2.SetTags(range(0, 151, 10))
-        #knob2.SetAngularRange(0, 270)
-        #knob2.SetValue(100)
-        #knob3.SetTags(range(0, 151, 10))
-        #knob3.SetAngularRange(0, 270)
-        #knob3.SetValue(100)
+        menuStaticSizer.Add(instrumentNameText, 0, wx.ALL|wx.EXPAND, 5)
+        menuStaticSizer.Add(instrumentNameTextCtrl, 2, wx.ALL|wx.EXPAND, 5)
+        menuStaticSizer.Add(instrumentColorText, 0, wx.ALL|wx.EXPAND, 5)
+        menuStaticSizer.Add(instrumentColorPicker, 0, wx.ALL|wx.EXPAND, 5)
+        menuStaticSizer.Add(saveButton, 0, wx.ALL|wx.EXPAND, 5)
 
-        #main_sizer = wx.BoxSizer(wx.VERTICAL)
-        #main_sizer.Add(hbox1, 1, wx.EXPAND)
-        #main_sizer.Add(knob1, 1, wx.EXPAND)
-        #main_sizer.Add(knob2, 1, wx.EXPAND)
-        #main_sizer.Add(knob3, 1, wx.EXPAND)
+        menusizer.Add(menuStaticSizer, 1, wx.ALL|wx.EXPAND, 5)
+        panelsizer.Add(menusizer, 0, wx.EXPAND|wx.ALL)
 
-        #self.SetSizer(main_sizer)
-        #main_sizer.Layout()
+        rightknobsizer.Add(self.knob1, 1, wx.ALL|wx.EXPAND, 5)
+        rightknobsizer.Add(self.knobtracker1, 0, wx.ALL, 5)
+        bottomsizer.Add(rightknobsizer, 1, wx.ALL|wx.EXPAND, 5)
+        middleknobsizer.Add(self.knob3, 1, wx.ALL|wx.EXPAND, 5)
+        middleknobsizer.Add(self.knobtracker3, 0, wx.ALL, 5)
+        bottomsizer.Add(middleknobsizer, 1, wx.ALL|wx.EXPAND, 5)
+        leftknobsizer.Add(self.knob2, 1, wx.ALL|wx.EXPAND, 5)
+        leftknobsizer.Add(self.knobtracker2, 0, wx.ALL, 5)
+        bottomsizer.Add(leftknobsizer, 1, wx.ALL|wx.EXPAND, 5)
+        panelsizer.Add(bottomsizer, 1, wx.EXPAND|wx.ALL, 20)
 
-        #s = wx.BoxSizer(wx.VERTICAL)
-        #s.Add(self, 1, wx.EXPAND)
-        #win.SetSizer(s)
+        self.SetSizer(panelsizer)
+        panelsizer.Layout()
+
+        self.Bind(KC.EVT_KC_ANGLE_CHANGED, self.OnAngleChanged1, self.knob1)
+        self.Bind(KC.EVT_KC_ANGLE_CHANGED, self.OnAngleChanged2, self.knob2)
+        self.Bind(KC.EVT_KC_ANGLE_CHANGED, self.OnAngleChanged3, self.knob3)
+
+    def OnAngleChanged1(self, event):
+
+        value = event.GetValue()
+        self.knobtracker1.SetLabel("Value = " + str(value))
+        self.knobtracker1.Refresh()
+
+    def OnAngleChanged2(self, event):
+
+        value = event.GetValue()
+        self.knobtracker2.SetLabel("Value = " + str(value))
+        self.knobtracker2.Refresh()
+
+    def OnAngleChanged3(self, event):
+
+        value = event.GetValue()
+        self.knobtracker3.SetLabel("Value = " + str(value))
+        self.knobtracker3.Refresh()
--- plugin.py
+++ plugin.py
@@ -2,11 +2,21 @@ from enum import Enum
 import wx
 
 
-class PluginBase(object):
+class PluginBase(wx.Panel):
     def __init__(self, frameParent, type, icon):
+        self.frameParent = frameParent
+        self.win = wx.MDIChildFrame(frameParent, -1, "Oscillator", size=(600,400),
+                               style=wx.DEFAULT_FRAME_STYLE ^ wx.MINIMIZE_BOX ^ wx.MAXIMIZE_BOX)
+
+        super(wx.Panel, self).__init__(self.win)
+        icn = wx.Icon()
+        icn.CopyFromBitmap(icon)
+        self.win.SetIcon(icn)
+
         self.frameParent = frameParent
         self.icon = icon
         self.pluginType = type
+        self.pluginName = "Oscillator"
         self.iconSize = (100,50)
 
 
