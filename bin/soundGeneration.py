c049e7166634fe91a6eb0df7e4c798b112e598a9
DESKTOP-UAECPSK\Qlien <bakalarskimichal@gmail.com>
2018-09-15 02:49:11 +0200

    fixes

--- bin/soundGeneration.py
+++ bin/soundGeneration.py
@@ -1,6 +1,8 @@
 import time
+from queue import Queue
 
 import numpy as np
+import pyaudio
 
 basicNotesKeys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 keys = [3, 4, 5, 6]
@@ -12,6 +14,7 @@ def frequencyFormula(n):
 
 
 class SoundGenerator:
+    soundChunksQueue = Queue()
     def __init__(self, sampling_rate=44100):
         self.sampling_rate = sampling_rate
         self.soundPlaying = False
@@ -30,7 +33,18 @@ class SoundGenerator:
         current_interval_value = 0
         number_of_frames = int(last_note_frame * self.sampling_rate * 60. / self.BPM)
         current_generators = []
+        SoundGenerator.soundChunksQueue = Queue()
 
+        p = pyaudio.PyAudio()
+        # for paFloat32 sample values must be in range [-1.0, 1.0]
+        stream = p.open(format=pyaudio.paFloat32,
+                        channels=1,
+                        rate=self.sampling_rate,
+                        output=True,
+                        frames_per_buffer=self.sampling_rate,
+                        stream_callback=self.pyAudioCallback)
+
+        stream.start_stream()
         ct = time.time()
         internal_wave_array = []
         r = int(self.sampling_rate * 60. / self.BPM)
@@ -38,13 +52,14 @@ class SoundGenerator:
             internal_buffer_counter = 0
             if current_interval_value % r == 0:
                 if len(internal_wave_array) > 0:
+                    self.soundChunksQueue.put(np.array(internal_wave_array).astype(np.float32))
                     SoundArray.extend(internal_wave_array)
                     internal_wave_array = []
                 if int(current_interval_value / r) in self.sounds:
                     current_generators.extend([x.plugin.generate_sound(
-                        frequency=frequencyFormula(x.verticalElementPosition)
+                        frequency=frequencyFormula(x.verticalElementPosition + firstKeyN)
                         , duration=(60. * x.length / self.BPM), sample_rate=self.sampling_rate
-                        , framesInterval=1024, bpm=128) for x in self.sounds[int(current_interval_value / r)]])
+                        , framesInterval=self.sampling_rate, bpm=128) for x in self.sounds[int(current_interval_value / r)]])
 
             generators_to_remove = []
             generators_counted = 0
@@ -66,9 +81,15 @@ class SoundGenerator:
             current_interval_value += 1
             internal_buffer_counter += 1
 
+        while stream.is_active():
+            time.sleep(0.5)
         print(time.time() - ct)
         self.soundPlaying = False
         pass
+
+    @staticmethod
+    def pyAudioCallback(in_data, frame_count, time_info, status):
+        return (SoundGenerator.soundChunksQueue.get(), pyaudio.paContinue)
 
     def update_sounds(self, soundElements):
         self.sounds = soundElements
